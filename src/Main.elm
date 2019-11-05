e2c2e98edbca4d7f3509dd5a956059b239d1eb0b
Matteo Gildone <matteo.gildone@springernature.com>
2019-11-05 14:03:18 +0000

    buttond

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,9 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, bool, decodeString, int, list, string, succeed)
+import Json.Decode.Pipeline exposing (hardcoded, required)
 
 
 
@@ -23,13 +26,58 @@ main =
 -- MODEL
 
 
+type alias Race =
+    { name : String
+    , pct : Int
+    , id : String
+    , selected : Bool
+    }
+
+
+type alias Races =
+    List Race
+
+
 type alias Model =
-    Int
+    { races : Maybe Races
+    , race : Maybe Race
+    , errors : Maybe Http.Error
+    }
+
+
+raceDecoder : Decoder Race
+raceDecoder =
+    succeed Race
+        |> required "name" string
+        |> required "pct" int
+        |> required "id" string
+        |> hardcoded False
+
+
+baseUrl : String
+baseUrl =
+    "/"
+
+
+initialModel : Model
+initialModel =
+    { races = Nothing
+    , race = Nothing
+    , errors = Nothing
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( 0, Cmd.none )
+    ( initialModel, fetchRaces )
+
+
+fetchRaces : Cmd Msg
+fetchRaces =
+    Http.get
+        { url = baseUrl ++ "data/races.json"
+        , expect = Http.expectJson LoadRaces (list raceDecoder)
+        }
 
 
 
@@ -37,18 +85,45 @@ init _ =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = LoadRaces (Result Http.Error Races)
+    | ToggleRace String
+
+
+updateRaceById : (Race -> Race) -> String -> Races -> Races
+updateRaceById updateRace id races =
+    List.map
+        (\race ->
+            if race.id == id then
+                updateRace race
+
+            else
+                race
+        )
+        races
+
+
+updateRaces : (Race -> Race) -> String -> Maybe Races -> Maybe Races
+updateRaces updateRace id maybeRaces =
+    Maybe.map (updateRaceById updateRace id) maybeRaces
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
+        LoadRaces (Ok races) ->
+            ( { model | races = Just races }
+            , Cmd.none
+            )
 
-        Decrement ->
-            ( model - 1, Cmd.none )
+        LoadRaces (Err error) ->
+            ( { model | errors = Just error }, Cmd.none )
+
+        ToggleRace id ->
+            ( { model
+                | races = updateRaces toggleRace id model.races
+              }
+            , Cmd.none
+            )
 
 
 
@@ -64,6 +139,38 @@ subscriptions model =
 -- VIEW
 
 
+toggleRace : Race -> Race
+toggleRace race =
+    { race | selected = not race.selected }
+
+
+viewRace : Race -> Html Msg
+viewRace race =
+    let
+        buttonClass =
+            if race.selected then
+                "c-button--unelevated"
+
+            else
+                "c-button--raised"
+    in
+    button [ class buttonClass, onClick (ToggleRace race.id) ] [ text race.name ]
+
+
+viewRaces : Maybe Races -> Html Msg
+viewRaces maybeRaces =
+    case maybeRaces of
+        Just race ->
+            div []
+                [ h3 [] [ text "Select your race" ]
+                , div [] (List.map viewRace race)
+                ]
+
+        Nothing ->
+            div [ class "loading-feed" ]
+                [ text "Loading Races..." ]
+
+
 view : Model -> Html Msg
 view model =
     div []
@@ -72,9 +179,5 @@ view model =
                 [ h1 [ class "c-nav-bar__logo" ] [ text "SOC" ]
                 ]
             ]
-        , div []
-            [ button [ onClick Decrement ] [ text "-" ]
-            , div [] [ text (String.fromInt model) ]
-            , button [ onClick Increment ] [ text "+" ]
-            ]
+        , viewRaces model.races
         ]
